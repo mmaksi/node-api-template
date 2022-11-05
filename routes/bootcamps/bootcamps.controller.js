d5a7985b642302a9cc3c9a521c717433b2ee433d
mmaksi <markmaksi@outlook.com>
2022-11-05 20:20:57 +0200

    Adding advanced filtering to bootcamps

--- routes/bootcamps/bootcamps.controller.js
+++ routes/bootcamps/bootcamps.controller.js
@@ -7,6 +7,7 @@ const {
   getBootcampsInRadius,
 } = require("../../models/Bootcamp.model");
 const asyncHandler = require("../../utils/asyncHandler");
+const bootcampsDatabas = require("../../models/Bootcamp.mongo");
 
 /**
  * Desc {Get all bootcamps}
@@ -14,7 +15,13 @@ const asyncHandler = require("../../utils/asyncHandler");
  * Access {Public}
  */
 const httpGetAllBootcamps = asyncHandler(async (req, res) => {
-  const bootcamps = await getAllBootcamps();
+  let queryStr = JSON.stringify(req.query);
+  queryStr = queryStr.replace(
+    /\b(gt|gte|lt|lte|in)\b/g,
+    (match) => `$${match}`
+  );
+  const query = await bootcampsDatabas.find(JSON.parse(queryStr));
+  const bootcamps = query;
   res
     .status(200)
     .json({ success: true, count: bootcamps.length, data: bootcamps });
